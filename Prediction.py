e9296e53f3c9054165d08afd7667650010430297
Pothana Venkata Mouli Murapaka <64841456+Mouli322@users.noreply.github.com>
2023-09-30 20:58:03 +0530

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pickle
 import json
-
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 # Load the trained model
 pickle_in = open('Real_Estate_Price_Prediction.pickle', 'rb')
 model = pickle.load(pickle_in)
